46bb6229eea42c2714219e7839010ce4bc3294a7
--replace-all <igorcastejon@dcc.ufmg.br>
2022-10-25 00:16:26 -0300

    Ajust index para rotas credit_card_use

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { Knex } from 'knex';
 import { userRouterProvider } from './user.routes';
 import { creditCardRouterProvider } from './credit_card.routes';
+import { creditCardUseRouterProvider } from './credit_card_use.routes';
 
 export const routerProvider = (db: Knex): Router => {
     const router: Router = Router();
     router.use(userRouterProvider(db));
     router.use(creditCardRouterProvider(db));
+    router.use(creditCardUseRouterProvider(db));
     return router;
 };
