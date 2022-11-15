29825623c06f6bb160f9cc56dfb2ce84c8f84870
--replace-all <igorcastejon@dcc.ufmg.br>
2022-11-15 17:14:05 -0300

    Testes unitários uso de cartão por cartão

--- tests/unit/credit_cards_use/credit_cards_use.repository.test.ts
+++ tests/unit/credit_cards_use/credit_cards_use.repository.test.ts
@@ -43,6 +43,25 @@ describe('Get credit cards uses by user', () => {
     });
 });
 
+describe('Get credit card uses by credit card', () => {
+    it('should return credit card uses of credit card when credit card has registered uses', async () => {
+        const expectedUses = [
+            {
+                credit_card_id: '1',
+                user_id: '1',
+                value: 10.2,
+                description: 'Teste',
+                authorization_code: 'Teste',
+            },
+        ];
+        expect(await repo.getByCreditCardId('1')).toEqual(expectedUses);
+    });
+
+    it('should return an exception error when credit card has no registered uses', async () => {
+        expect(async () => await repo.getByUserId('3')).rejects.toThrow('Nao ha usos para esse usuario');
+    });
+});
+
 describe('Get all credit card uses', () => {
     it('should return all credit card uses', async () => {
         const expectedUses = [
