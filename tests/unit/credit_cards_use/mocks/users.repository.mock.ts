85269ed933244c7671ec40e767e238d3a9062a6e
Daniel Abadi <48056794+danielabadi@users.noreply.github.com>
2022-11-15 12:41:26 -0300

    Mock e testes de user

---
+++ tests/unit/credit_cards_use/mocks/users.repository.mock.ts
@@ -0,0 +1,42 @@
+import { IUserDAO } from '../../../../src/users/user.dao';
+import { CreateUserDTO, User, UserId } from '../../../../src/users/user.model';
+
+export class UserDAOMock implements IUserDAO {
+    private users = {
+        '1': {
+            id: '1',
+            name: 'Teste',
+            password: 'Teste',
+            role: 'Admin'
+        },
+        '2': {
+            id: '2',
+            name: 'Teste2',
+            password: 'Teste2',
+            role: 'User'
+        }
+    }
+
+    public async createUser(user: CreateUserDTO): Promise<string> {
+        if (user['name'] == 'Teste') {
+            return '1';
+        }
+        return '2';
+    }
+    public async getUserById(userId: UserId): Promise<User> {
+        if (userId === '1') {
+            return this.users['1'];
+        }
+        throw new Error('Nao ha usarios com esse id');
+    }
+    public async getUserByName(userName: string): Promise<User> {
+        if (userName === 'Teste') {
+            return this.users['1'];
+        }
+        throw new Error('Nao ha usuarios com esse nome');
+    }
+    public async getAllUsers(): Promise<User[]> {
+        return [this.users['1'], this.users['2']];
+    }
+
+}
---
+++ tests/unit/credit_cards_use/users.repository.test.ts
@@ -0,0 +1,66 @@
+import {
+    UserRepositoryProvider,
+    IUserRepository,
+} from '../../../src/users/user.repository';
+import { UserDAOMock } from './mocks/users.repository.mock';
+
+let repo: IUserRepository;
+beforeEach(() => {
+    repo = UserRepositoryProvider.create(new UserDAOMock());
+});
+
+describe('User creation', () => {
+    it('should return id of user when user is created successfully', async () => {
+        const toAdd = {
+            id: '1',
+            name: 'Teste',
+            password: 'Teste',
+            role: 'Admin'
+        };
+        const id_created = await repo.add(toAdd);
+        const isIdANumber = !isNaN(Number(id_created));
+        expect(isIdANumber).toBe(true);
+    });
+});
+
+describe('Get user', () => {
+    it('should return user by id', async () => {
+        const expectedUser = {
+                id: '1',
+                name: 'Teste',
+                password: 'Teste',
+                role: 'Admin'
+            };
+        expect(await repo.getById(expectedUser.id)).toEqual(expectedUser);
+    });
+
+    it('should return user by name', async () => {
+        const expectedUser = {
+                id: '1',
+                name: 'Teste',
+                password: 'Teste',
+                role: 'Admin'
+            };
+        expect(await repo.getByName(expectedUser.name)).toEqual(expectedUser);
+    });
+});
+
+describe('Get all users', () => {
+    it('should return all users', async () => {
+        const expectedUsers = [
+            {
+                id: '1',
+                name: 'Teste',
+                password: 'Teste',
+                role: 'Admin'
+            },
+            {
+                id: '2',
+                name: 'Teste2',
+                password: 'Teste2',
+                role: 'User'
+            }
+        ];
+        expect(await repo.getAll()).toEqual(expectedUsers);
+    });
+});
